ab12a680782d037ce93a5c645e777691b51943b9
parkbyunggwon <startartart@naver.com>
2023-02-01 19:56:51 +0900

    FEAT: WelcomeForm 컴포넌트 스타일링 및 배치

--- src/Welcome-components/WelcomeForm.js
+++ src/Welcome-components/WelcomeForm.js
@@ -1,28 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const Circle = styled.div`
-    position: absolute;
-    background-color: #fff;
+const WelcomeText = styled.p`
+    font-size: 40px;
+    font-weight: bold;
+    background: -webkit-linear-gradient(#35D6ED, #C9F6FF);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
     text-align: center;
-    border-radius: 50%;
-    background: lightblue;
-    top: 35%;
-    left: 50%;
-    width: 300px;
-    height: 300px;
-    transform: translate(-50%, -50%);
-    @media screen and (min-width: 600px) {
-        width: 500px;
-        height: 500px;
+    position: absolute;
+    top: 2.5%;
+    left: 20%;
+    &:hover {
+        background: -webkit-linear-gradient(#C9F6FF, #35D6ED);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
     }
-
+    //hover animation
+    animation: pulse 2s infinite;
+    @keyframes pulse {
+        0% {
+            transform: scale(1);
+        }
+        50% {
+            transform: scale(1.05);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
 `;
 
-const WelcomeForm = () => {
+const CheckText = styled.p`
+    font-size: 20px;
+    font-weight: bold;
+    background: -webkit-linear-gradient(#35D6ED, #C9F6FF);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    
+    /* &:hover {
+        animation: slide_left_right 1s infinite;
+        @keyframes slide_left_right {
+            0% {
+                transform: translateX(0);
+            }
+            50% {
+                transform: translateX(50px);
+            }
+            100% {
+                transform: translateX(0);
+            }
+        }
+    } */
+
+    //disappear is true then scatter
+    ${props => props.disappear && `
+        animation: scatter 0.5s ease-in-out forwards;
+        @keyframes scatter {
+            from {
+                opacity: 1;
+                scale: 1;
+            }
+            to {
+                opacity: 0;
+                scale: 0;
+            }
+        }
+    `}
+   
+`;
+
+const CheckTextBox = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 350px;
+    height: 250px;
+`;
+
+const WelcomeForm = ({showApp, login, guest, take}) => {
+
+    const [disappear, setDisappear] = useState(showApp);
+
+    const takeHandler = (e) => {
+        setDisappear(!disappear);
+        take(e.target.id);
+    }
+
+
     return (
         <>
-            <Circle />
+            <WelcomeText>IVIS</WelcomeText>
+            <CheckTextBox>
+            <CheckText disappear={showApp} number={1} id={1} onClick={takeHandler}>2023 신입부원 모집</CheckText>
+            <CheckText disappear={showApp} number={2} id={2} onClick={takeHandler}>Main page</CheckText>
+            <CheckText disappear={showApp} number={3} id={3} onClick={takeHandler}>About</CheckText>
+            </CheckTextBox>
         </>
     );
 }
