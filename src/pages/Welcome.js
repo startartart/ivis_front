0b229308cc679a963c3799cc3dfd10fc0be1c533
parkbyunggwon <startartart@naver.com>
2023-02-02 21:41:50 +0900

    FEAT: Add SlidingDiv, login status to MainForm

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,22 +2,50 @@ import React, { useState } from 'react';
 import WelcomeForm from '../Welcome-components/WelcomeForm';
 import SignUpForm from '../Welcome-components/SignUpForm';
 import ParticleBackground from 'react-particle-backgrounds'
+import MainForm from './MainForm';
+import styled from 'styled-components';
+
+const SlidingDiv = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    margin: 0;
+
+    ${props => props.status && `
+        animation: slideDown 1s ease-in-out;
+    `}
+
+    @keyframes slideDown {
+        0% {
+            transform: translateY(0%);
+        }
+        100% {
+            transform: translateY(-100%);
+        }
+    }
+`;
+
 
 const Welcome = () => {
     const [showApp, setShowApp] = useState(false);
     const [login, setLogin] = useState(false);
     const [guest, setGuest] = useState(false);
 
-    const take = (e) => {
+    const take = async (e) => {
         if (e === "1") {
             //toggle showApp
             setShowApp(!showApp);
         } else if (e === "2") {
-            setLogin(true);
+            //go to MainForm page
+            setGuest(true);
+            //5ms after Guest true
+            setTimeout(() => {
+                setLogin(true);
+            }, 1000);
         } else if (e === "3") {
             setGuest(true);
         }
-        
     }
 
     const settings = {
@@ -47,9 +75,15 @@ const Welcome = () => {
       
     return (
         <>
-            <ParticleBackground settings={settings}/>
-            <WelcomeForm showApp={showApp} login={login} guest={guest} take={take}/>
-            {showApp && <SignUpForm showApp={showApp} take={take} />}
+            {!login &&
+                <SlidingDiv status={guest}>
+                    <ParticleBackground settings={settings}/>
+                    <WelcomeForm showApp={showApp} login={login} guest={guest} take={take}/>
+                    {showApp && <SignUpForm showApp={showApp} take={take} />}
+                </SlidingDiv>
+            }
+            
+            {login && <MainForm />}
         </>
 
     );
