a948b9921f71ba2367a8bd6c67c2944230f9576a
parkbyunggwon <startartart@naver.com>
2023-02-16 20:53:15 +0900

    FEAT: Main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,26 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import './Main.scss';
+import MainForm from "../Main-components/MainForm";
+import About from "../Main-components/About";
+import Member from "../Main-components/Member";
+import Services from "../Main-components/Services";
+import Menu from "../Main-components/Menu";
+import Footer from '../Main-components/Footer';
 
 const Main = () => {
   return (
-    <div>
-      <h1>Main</h1>
+    <div className='main-background'>
+      <Menu />
+      <div className='pages'>
+      <Routes>
+          <Route path="/" element={<MainForm />} />
+          <Route path="about" element={<About />} />
+          <Route path="member" element={<Member />} />
+          <Route path="services" element={<Services />} />
+      </Routes>
+      <Footer />
+      </div>
     </div>
   );
 }
